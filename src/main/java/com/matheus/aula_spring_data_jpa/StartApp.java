200192e5c60adb11fb85adb07287978572e2a44e
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-07-25 00:48:53 -0300

    Aula JPA Repository

--- src/main/java/com/matheus/aula_spring_data_jpa/StartApp.java
+++ src/main/java/com/matheus/aula_spring_data_jpa/StartApp.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
 
@@ -15,16 +17,21 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User();
-        user.setName("Maria");
-        user.setUsername("maria");
-        user.setPassword("maria123");
-
-        repository.save(user);
-
-        for (User u: repository.findAll()) {
+        List<User> users = repository.findByNameContaining("Matheus");
+        for (User u : users) {
             System.out.println(u);
         }
+    }
 
+    private void insertUser() {
+        User user = new User();
+        user.setName("GABRIEL NUNES");
+        user.setUsername("gabriel");
+        user.setPassword("santos");
+        repository.save(user);
+
+        for (User u : repository.findAll()) {
+            System.out.println(u);
+        }
     }
 }
--- src/main/java/com/matheus/aula_spring_data_jpa/repository/UserRepository.java
+++ src/main/java/com/matheus/aula_spring_data_jpa/repository/UserRepository.java
@@ -2,6 +2,17 @@ package com.matheus.aula_spring_data_jpa.repository;
 
 import com.matheus.aula_spring_data_jpa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findByNameContaining(String name);
+
+    User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name") String name);
 }
